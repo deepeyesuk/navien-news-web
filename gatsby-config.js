12542f1a6f1a99a00fbb5aa931dd6013051237e4
hyungkyu <hyungkyu@deepeyes.co.uk>
2022-07-22 21:01:55 +0100

    typescript ready on gatsby configuration

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://mobile-news.navienuk.net",
+    title: "Navien Mobile News",
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+  ],
+};
