0c457ca77161f66e428bdf004f3257b830fc09b2
Andrew Youngho Chaa <andrew.yh.chaa@gmail.com>
2022-08-09 10:02:15 +0100

    Removed image

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -98,21 +98,9 @@ const BlogPostTemplate = ({
           />
         </div>
 
-        <hr />
-          
-        <figure className="my-2">
-          <img
-            className="w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-            alt=""
-            width={1310}
-            height={873}
-          />
-          <figcaption>Source : consectetur quam.</figcaption>
-        </figure>
 
       </div>
-      
+
 
       <nav
         className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
